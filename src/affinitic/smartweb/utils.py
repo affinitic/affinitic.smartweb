7d309f5659a2f5dd55bc80c62a433ac058bde5a9
jimbiscuit <julienchandelle@gmail.com>
2023-04-25 09:43:36 +0200

    Fix removing default folder

--- src/affinitic/smartweb/utils.py
+++ src/affinitic/smartweb/utils.py
@@ -7,15 +7,14 @@ from Products.CMFCore.interfaces import IFolderish
 
 def delete_i_am_i_find_folders(context):
     # TODO: context could be the Plone Site OR a Language Root Folder; handle both cases
-
+    portal = api.portal.get()
     normalizer = getUtility(IIDNormalizer)
     lang = api.portal.get_current_language()[:2]
-
     for title in ("I am", "I find"):
         translated_title = translate(_isw(title), target_language=lang)
         translated_id = normalizer.normalize(translated_title)
-        if translated_id in context:
-            api.content.remove(id=translated_id)
+        if translated_id in portal:
+            api.content.delete(obj=portal[translated_id])
 
 def check_if_folder_exist(context, folder_id):
     if getattr(context, folder_id, False):
