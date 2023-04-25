1034fc6b2d7a613d090906ed83759d0f0585ce14
jimbiscuit <julienchandelle@gmail.com>
2023-04-25 09:44:00 +0200

    Locals

--- src/affinitic/smartweb/locales/update.sh
+++ src/affinitic/smartweb/locales/update.sh
@@ -7,6 +7,10 @@
 # (e.g. locales/de/LC_MESSAGES/affinitic.smartweb.po)
 
 domain=affinitic.smartweb
+domain_plone=plone
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+i18ndude rebuild-pot --pot $domain_plone.pot --create $domain_plone ../
+i18ndude sync --pot $domain_plone.pot */LC_MESSAGES/$domain_plone.po
