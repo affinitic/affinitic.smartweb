2d910a69657f82a8b588546b1b6dbac7596a7dd6
jimbiscuit <julienchandelle@gmail.com>
2023-04-25 09:36:50 +0200

    Add custom hook for migration

--- src/affinitic/smartweb/utils.py
+++ src/affinitic/smartweb/utils.py
@@ -3,7 +3,7 @@ from plone.i18n.normalizer import IIDNormalizer
 from plone import api
 from zope.component import getUtility
 from zope.i18n import translate
-
+from Products.CMFCore.interfaces import IFolderish
 
 def delete_i_am_i_find_folders(context):
     # TODO: context could be the Plone Site OR a Language Root Folder; handle both cases
@@ -16,3 +16,12 @@ def delete_i_am_i_find_folders(context):
         translated_id = normalizer.normalize(translated_title)
         if translated_id in context:
             api.content.remove(id=translated_id)
+
+def check_if_folder_exist(context, folder_id):
+    if getattr(context, folder_id, False):
+        if IFolderish.providedBy(context[folder_id]):
+            return True
+        else:
+            raise TypeError(f"Item {folder_id} exist but not a folder")
+    else:
+        return False
